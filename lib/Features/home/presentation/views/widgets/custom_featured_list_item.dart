806d958e426b0a1da4c57bc4654c8197f778b80e
KyrillosAymanBotros <kero40054@gmail.com>
2026-03-09 03:16:01 +0200

    Add CustomFeaturedListItem widget and integrate it into HomeViewBody

---
+++ lib/Features/home/presentation/views/widgets/custom_featured_list_item.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/Core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+class CustomFeaturedListItem extends StatelessWidget {
+  const CustomFeaturedListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * .23,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Core/utils/assets_data.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_featured_list_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -11,25 +12,4 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class CustomFeaturedListItem extends StatelessWidget {
-  const CustomFeaturedListItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.sizeOf(context).height * .23,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              image: AssetImage(AssetsData.testImage),
-              fit: BoxFit.fill,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
