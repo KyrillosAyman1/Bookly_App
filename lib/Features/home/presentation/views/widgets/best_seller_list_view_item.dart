a72f8e53af5d7a844f68481725397eb37bbc4647
KyrillosAymanBotros <kero40054@gmail.com>
2026-03-12 03:12:58 +0200

    Add Best Seller List View and refactor HomeViewBody layout

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -3,72 +3,67 @@ import 'package:bookly_app/Core/utils/assets_data.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 24),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 24),
-            child: SizedBox(
-              height: 125,
-              child: AspectRatio(
-                aspectRatio: 2.5 / 4,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    image: const DecorationImage(
-                      image: AssetImage(AssetsData.testImage),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 125,
+          child: AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
                 ),
               ),
             ),
           ),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: Text(
-                    "Harry Potter and the Goblet of Fire",
-                    style: AppStyles.textStyle20.copyWith(
-                      fontFamily: kGtSectraFine,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+        ),
+        const SizedBox(width: 30),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  "Harry Potter and the Goblet of Fire",
+                  style: AppStyles.textStyle20.copyWith(
+                    fontFamily: kGtSectraFine,
                   ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                const SizedBox(height: 3),
-                const Text("J.K. Rowling", style: AppStyles.textStyle14),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text(
-                      "19.99\$",
-                      style: AppStyles.textStyle20.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
+              ),
+              const SizedBox(height: 3),
+              const Text("J.K. Rowling", style: AppStyles.textStyle14),
+              const SizedBox(height: 3),
+              Row(
+                children: [
+                  Text(
+                    "19.99\$",
+                    style: AppStyles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                
                     ),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                  const Spacer(),
+                  const BookRating(),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,8 @@
 import 'package:bookly_app/Core/utils/app_styles.dart';
-import 'package:bookly_app/Core/utils/assets_data.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
-import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -11,20 +10,20 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-        SizedBox(height: 40),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Text("Best Seller", style: AppStyles.textStyle18),
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: CustomAppBar()),
+        SliverToBoxAdapter(child: FeaturedBooksListView()),
+        SliverToBoxAdapter(child: SizedBox(height: 40)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Text("Best Seller", style: AppStyles.textStyle18),
+          ),
         ),
-        SizedBox(height: 20),
-        BestSellerListViewItem(),
+        SliverToBoxAdapter(child: SizedBox(height: 20)),
+        BestSellerListView(),
       ],
     );
   }
 }
-
