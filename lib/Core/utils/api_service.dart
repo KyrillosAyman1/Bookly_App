0257e131a7333f32cf16f37b1a289572903373fa
KyrillosAymanBotros <kero40054@gmail.com>
2026-04-21 05:27:45 +0200

    Refactor ApiService and HomeRepoImpl; implement fetchNewestBooks method and add fetchFutureBooks to HomeRepo

--- lib/Core/utils/api_service.dart
+++ lib/Core/utils/api_service.dart
@@ -3,12 +3,12 @@ import 'package:dio/dio.dart';
 class ApiService {
   final String _baseUrl = "https://www.googleapis.com/books/v1/";
   String _apiKey = "AIzaSyC41Q0jtqpgo9tCcS-u-0aMO-ULFmvstNY";
-  final Dio dio;
+  final Dio _dio;
 
-  ApiService(this.dio);
+  ApiService(this._dio);
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
-    var response = await dio.get("$_baseUrl$endpoint&key=$_apiKey");
+    var response = await _dio.get("$_baseUrl$endpoint&key=$_apiKey");
     return response.data;
   }
 }
