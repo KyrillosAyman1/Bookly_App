348cd2a066e738f85dd12d8da22cad54d41d3f58
KyrillosAymanBotros <kero40054@gmail.com>
2026-04-21 04:33:14 +0200

    Add ApiService and HomeRepoImpl classes; include dio dependency in pubspec.yaml

---
+++ lib/Core/utils/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final String _baseUrl = "https://www.googleapis.com/books/v1/";
+  String _apiKey = "AIzaSyC41Q0jtqpgo9tCcS-u-0aMO-ULFmvstNY";
+  final Dio dio;
+
+  ApiService(this.dio);
+
+  Future<Map<String, dynamic>> get({required String endpoint}) async {
+    var response = await dio.get("$_baseUrl$endpoint&key=$_apiKey");
+    return response.data;
+  }
+}
