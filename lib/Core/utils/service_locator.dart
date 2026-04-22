de8664e581b15cf8cf81b63b33daaddecccea6e2
KyrillosAymanBotros <kero40054@gmail.com>
2026-04-22 03:26:59 +0200

    Add service locator setup and include get_it dependency

---
+++ lib/Core/utils/service_locator.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/Core/utils/api_service.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton(ApiService(Dio()));
+  getIt.registerSingleton( HomeRepoImpl(
+        getIt.get<ApiService>(),
+      ),
+      );
+}
